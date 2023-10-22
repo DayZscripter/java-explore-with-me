b8c603f8edaa79815a881061036f8775df5fd557
DayZscripter <jkrehabjk@gmail.com>
2023-10-23 00:58:51 +0300

    Этап 2. Основной сервис (add dto,controllers, endpoint)

---
+++ main-service/src/main/java/ru/practicum/ewm/dto/users/UserDto.java
@@ -0,0 +1,24 @@
+package ru.practicum.ewm.dto.users;
+
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+
+    @Email
+    @Length(min = 6, max = 254)
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Length(min = 2, max = 250)
+    private String name;
+}
---
+++ stats-service/stats-dto/src/main/java/ru/practicum/ewm/ConstantsForDto.java
@@ -0,0 +1,10 @@
+package ru.practicum.ewm;
+
+public final class ConstantsForDto {
+
+    public static final String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    private ConstantsForDto() {
+
+    }
+}
